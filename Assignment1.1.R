a2b8406d8a8ab53c78635f85de9090f6e7e30cf0
Srikanth K <sri.suku07@gmail.com>
2019-02-17 19:33:35 +0530

    Add files via upload

---
+++ Assignment1.1.R
@@ -0,0 +1,11 @@
+#2. What is Recycling of elements in a vector?
+
+v1<-c(1,2,3)    
+v2<-c(6,7,8,9,10) 
+v1 + v2
+
+
+#3. Give an example of recycling of elements.
+v1<-c(1,2,3)    
+v2<-c(1,2,3,4,5,6,7,8,9,10) 
+v1 + v2
